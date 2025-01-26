75d719b3c5f820bd33b629d46059622c0ea7c5be
NIGHT-OWL01 <ganeshp7754@gmail.com>
2025-01-26 11:25:40 +0530

    get css properties of element by using window obj

--- 04 DOM/01dom/script.js
+++ 04 DOM/01dom/script.js
@@ -1,16 +1,38 @@
-let counter = document.querySelector(".counter");
-let followers = document.querySelector(".followers");
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
 
-let count=1;
+const center = document.querySelector(".center");
 
-setInterval(()=>{
-    if(count<1000){
-        count++;
-        counter.innerHTML=count;
-    }
-    if(counter.innerText=="1000"){
-        counter.innerText=`${counter.innerHTML} followers on instagram`
-    }
-},1)
+// console.log(window.getComputedStyle(red));
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGColor=(element)=>{
+    // console.log(window.getComputedStyle(element).backgroundColor);
+    return window.getComputedStyle(element).backgroundColor;
+}
+
+
+console.log(getBGColor(pink));
+
+
+
+
+// let counter = document.querySelector(".counter");
+// let followers = document.querySelector(".followers");
+
+// let count=1;
+
+// setInterval(()=>{
+//     if(count<1000){
+//         count++;
+//         counter.innerHTML=count;
+//     }
+//     if(counter.innerText=="1000"){
+//         counter.innerText=`${counter.innerHTML} followers on instagram`
+//     }
+// },1)
 
 
